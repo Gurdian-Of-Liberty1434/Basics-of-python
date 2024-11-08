1302ac526c926ed14fbf5af020bc62fb3c2ab4af
Gurdian-Of-Liberty1434 <samurdika.sajeewani88@gmail.com>
2024-11-08 16:01:57 +0530

    Add files via upload

---
+++ Welcome to the coding world.py/Welcome.py
@@ -0,0 +1,3 @@
+print("my job is","a military general","for super earth", sep=",", end="// ");
+print("I", "HATE", "MY", "JOB", sep="*", end=">:( ");
+print("Joey is a quirky soldier who always dies by my tactics")
