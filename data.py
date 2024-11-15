2609732150b19b70e3a0410efab91ae17dc41cb1
Gurdian-Of-Liberty1434 <samurdika.sajeewani88@gmail.com>
2024-11-15 15:49:16 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,8 @@
+a="true"
+b=1.2
+c=2
+d=input("Enter a number")
+print("The type of a is", type(a))
+print('The type of b is', type(b))
+print('The type of c is', type(c))
+print('The type of d is', type(d))
