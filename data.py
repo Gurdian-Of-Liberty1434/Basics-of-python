d176bb8af68c742cfdf7f46dd05088c4dbaa1043
Gurdian-Of-Liberty1434 <samurdika.sajeewani88@gmail.com>
2024-11-15 15:59:33 +0530

    Add files via upload

--- data.py
+++ data.py
@@ -2,7 +2,11 @@ a="true"
 b=1.2
 c=2
 d=input("Enter a number")
+e=input("enter anything")
+f=bool(e)
 print("The type of a is", type(a))
 print('The type of b is', type(b))
 print('The type of c is', type(c))
 print('The type of d is', type(d))
+print('The type of e (before type casting) is', type(e))
+print('The type of f (after type casting) is', type(f))
