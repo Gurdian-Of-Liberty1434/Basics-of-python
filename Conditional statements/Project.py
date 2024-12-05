d40853fb78e85460c2eb7eb948ddcd5bd054fd55
Gurdian-Of-Liberty1434 <samurdika.sajeewani88@gmail.com>
2024-12-05 21:25:35 +0530

    Add files via upload

---
+++ Conditional statements/Project.py
@@ -0,0 +1,17 @@
+date=str(input("enter date(NO YEARS)"))
+
+
+
+
+
+if date=="21th of june":
+    print("Summer")
+
+if date=="21th of october":
+    print("Autumn")
+
+if date=="21th of february":
+    print("Winter")
+
+if date=="21th of december":
+    print("Spring")
