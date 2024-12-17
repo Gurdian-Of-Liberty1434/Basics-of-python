8f14869921628da6bfc3e68448f4b3423b99b504
Gurdian-Of-Liberty1434 <samurdika.sajeewani88@gmail.com>
2024-12-17 22:02:24 +0530

    Add files via upload

---
+++ Nested conditional statements/project.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age"))
+if age < 20 and age > 10:
+    print("You are welcome to come to my class but better get better grades for ur exam or else...")
+elif age < 10:
+    print("GET OUT OF MY CLASS NOW YOUNG BOI OR I WILL HIT U WITH MY RULER")
+else:
+    print("I THOUGHT U ADULTS ARE EDUCATED, GET OUT NOOOWWWWWWWWWWWWWWW")
