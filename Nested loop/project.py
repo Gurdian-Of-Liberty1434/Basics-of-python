692a839cac169bad71ebb5317bf7a30a500f7730
Gurdian-Of-Liberty1434 <samurdika.sajeewani88@gmail.com>
2025-01-03 21:02:55 +0530

    Add files via upload

---
+++ Nested loop/project.py
@@ -0,0 +1,10 @@
+#Write a program to cenvert a decimal number into binary number
+num=int(input("Enter a number"))
+binary=0
+i=1
+while num>0:
+    rem=num%2
+    binary=binary+rem*i
+    num=num//2
+    i=i*10
+print(binary)
