e9ed10b3cce469e90c31200e5193a12dddc1d9b2
Gurdian-Of-Liberty1434 <samurdika.sajeewani88@gmail.com>
2025-02-04 19:31:48 +0530

    Add files via upload

---
+++ Arguements/shutdown.py
@@ -0,0 +1,15 @@
+
+
+choice=input("Do you want to initiate shutdown? : ")
+
+if choice=="Yes":
+    print("Initiating shutdown.....")
+elif choice=="No":
+    print("Shutdown protocol cancelled")
+elif choice!="yes":
+    print("INPUT INCORRECT... please type 'Yes' or 'No'")
+elif choice!="No":
+    print("INPUT INCORRECT... please type 'Yes' or 'No'")
+
+
+
